734987aacb9ee78bb00ee48a1c97b3af40a52d53
Flavioeds82 <flavio_eduardo@id.uff.br>
2022-10-24 21:36:09 -0300

    Implemented cart layout

--- food-devs/src/components/Cart/index.tsx
+++ food-devs/src/components/Cart/index.tsx
@@ -2,8 +2,11 @@ import React, { ReactNode, Reducer, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Container } from "./styled";
 import cart from "../../assets/images/cart.png";
+import minus from "../../assets/images/minus.png";
+import plus from "../../assets/images/plus.png";
 import down from "../../assets/images/down.png";
-import { initialStateCart } from '../../types';
+import edit from "../../assets/images/edit.png";
+import { initialStateCart, Product } from '../../types';
 import { CombinedState } from 'redux';
 
 
@@ -31,7 +34,56 @@ export const Cart: React.FC<indexProps> = () => {
             
             {active && products.length > 0 &&
                <div className="cart-body">
-                  <h1>Cart Body</h1>
+                  {products.map((item: Product, index: number)=>
+                  
+                     <div className="cart-products" key={index}>
+                        <div className="cart-product-image">
+                           <img src={item.image} alt="" />
+                        </div>
+                        <div className="container-info">
+                           <div className="cart-product-info">
+                              <div className="title">{item.name}</div>
+                              <div className="price">{`R$ ${(item.price*item.qt).toFixed(2)}`}</div>
+                           </div>
+                           <div className="product-quant">
+                              <div className="minus"><img src={minus} alt="" /></div>
+                              <div className="number-qt">{item.qt}</div>
+                              <div className="plus"><img src={plus} alt="" /></div>
+                           </div>
+                        </div>
+                        
+                     </div>
+                     
+                  )}
+                  
+                  <div className="address-title">Entrega</div>
+                  <div className="address-container">
+                     <div className="address-info">
+                        <div className="addres-name">Minha Casa</div>
+                        <div className="addres-street">Rua A -  Nº 123</div>
+                        <div className="address-city">Itaguaí / Rj</div>
+                     </div>
+                     <div className="edit-address-icon"><img src={edit} alt="" /></div>
+                  </div>
+                  <div className="discount-title">Cupon de desconto</div>
+                  <div className="discount-input">
+                     <input type="text" />
+                  </div>
+                  <div className="cart-subtotal">
+                     <div className="subtotal">
+                        <div className="label">Desconto</div>
+                        <div className="value">R$ 0,00</div>
+                     </div>
+                     <div className="subtotal">
+                        <div className="label">Taxa de Entrega</div>
+                        <div className="value">R$ 0,00</div>
+                     </div>
+                     <div className="subtotal">
+                        <div className="label">Total</div>
+                        <div className="value">R$ 0,00</div>
+                     </div>
+                  </div>
+                  <div className="checkout">FINALIZAR COMPRA</div>
                </div>
             }
             
--- food-devs/src/components/Cart/styled.ts
+++ food-devs/src/components/Cart/styled.ts
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-   width: 18rem;
-   max-height: 80vh;
+   width: 23rem;
+   max-height: 75vh;
    background-color: rgba(19,103,18,150);
    position: fixed;
    bottom: 0;
    right: 3rem;
    border-radius: 0.3rem 0.3rem 0 0;
+   overflow: auto;
   .container{
      display: flex;
      flex-direction: column;
@@ -32,12 +33,111 @@ export const Container = styled.div`
         .arrow-down{
          img{
             width: 1rem;
-            margin-left: 3rem;
+            margin-left: 5rem;
          }
         }
      }
      .cart-body{
         min-height: 40vh;
+        margin: 1rem;
+        color: #fff;
+        
+
+        .cart-products{
+         display: flex;
+         width: 100%;
+         margin-bottom: 2rem;
+
+         .cart-product-image img{
+           height: 3.5rem;
+            border-radius: 0.8rem;
+         }
+         .container-info{
+            display: flex;
+            width: 100%;
+            justify-content: space-between;
+            .cart-product-info{
+               margin-left: 1rem;
+               text-align: left;
+
+               .title{
+                  font-weight: bold;
+                  font-size: 1rem;
+                  line-height: 1rem;
+                  margin-bottom: 0.5rem;
+               }
+               .price{
+                  font-size: 0.8rem;
+
+               }
+            }
+            .product-quant{
+               display: flex;
+               align-items: center;
+               font-weight: bold;
+               text-align: right;
+               width: 5rem;
+               margin-left: 1rem;
+               .number-qt{
+                  margin: 0 1rem;
+               }
+               img{
+                  width: 1rem;
+               }
+            }
+         
+         }
+        }
+        .address-title{
+         font-weight: bold;
+         text-align: left;
+        }
+        .address-container{
+         display: flex;
+         justify-content: space-between;
+         margin-bottom: 2rem;
+
+         .address-info{
+            text-align: left;
+            font-size: 0.8rem;
+         }
+         .edit-address-icon img{
+            width: 1rem;
+         }
+
+        }
+        .discount-title{
+         font-weight: bold;
+         text-align: left;
+         margin-bottom: 0.5rem;
+        }
+        .discount-input{
+         width: 100%;
+         margin-bottom: 2rem;
+         input{
+            width: 100%;
+            background-color: #FFF;
+            border: none;
+            height: 1.5rem;
+            border-radius: 0.5rem;
+         }
+        }
+        .cart-subtotal{
+         width: 100%;
+         margin-bottom: 2rem;
+         .subtotal{
+            display: flex;
+            justify-content: space-between;
+            font-weight: bold;
+         }
+
+        }
+        .checkout{
+         background-color: #073d07;
+         padding: 0.8rem;
+         border-radius: 0.8rem;
+
+        }
      }
   }
 `;
